c5f1c2334bc972d9f0e41c18e5299b3c76b362c6
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2023-11-16 20:33:22 -0500

    Initial create migration

---
+++ Migrations/20231117011453_InitialCreate.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ranking_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tierlists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    TemplateId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tierlists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Element",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Image = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    TemplateModelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Element", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Element_Templates_TemplateModelId",
+                        column: x => x.TemplateModelId,
+                        principalTable: "Templates",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tier",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Rank = table.Column<int>(type: "integer", nullable: false),
+                    TemplateModelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tier", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tier_Templates_TemplateModelId",
+                        column: x => x.TemplateModelId,
+                        principalTable: "Templates",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RankedElement",
+                columns: table => new
+                {
+                    ElementId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TierId = table.Column<int>(type: "integer", nullable: false),
+                    Position = table.Column<int>(type: "integer", nullable: false),
+                    TierlistModelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RankedElement", x => x.ElementId);
+                    table.ForeignKey(
+                        name: "FK_RankedElement_Tierlists_TierlistModelId",
+                        column: x => x.TierlistModelId,
+                        principalTable: "Tierlists",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Element_TemplateModelId",
+                table: "Element",
+                column: "TemplateModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RankedElement_TierlistModelId",
+                table: "RankedElement",
+                column: "TierlistModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tier_TemplateModelId",
+                table: "Tier",
+                column: "TemplateModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Element");
+
+            migrationBuilder.DropTable(
+                name: "RankedElement");
+
+            migrationBuilder.DropTable(
+                name: "Tier");
+
+            migrationBuilder.DropTable(
+                name: "Tierlists");
+
+            migrationBuilder.DropTable(
+                name: "Templates");
+        }
+    }
+}
