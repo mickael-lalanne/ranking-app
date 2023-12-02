6cdc1a1af33ae290cba93f1fc7fc594cf933d733
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2023-12-02 07:30:09 -0500

    Client: add Clerk library to handle authentication

---
+++ ClientApp/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
+import { Layout } from './components/Layout';
+import './custom.css';
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key');
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <ClerkProvider publishableKey={clerkPubKey}>
+                <Layout>
+                    <SignedIn>
+                        <Routes>
+                            {AppRoutes.map((route, index) => {
+                                const { element, ...rest } = route;
+                                return (
+                                    <Route key={index} {...rest} element={element} />
+                                );
+                            })}
+                        </Routes>
+                    </SignedIn>
+
+                    <SignedOut>
+                        <RedirectToSignIn />
+                    </SignedOut>
+                </Layout>
+            </ClerkProvider>
+        );
+    }
+}
