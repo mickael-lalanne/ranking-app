a17819802d9705f6bec528c1724370e0d414ef2d
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2023-12-17 16:41:50 -0500

    Use environment variable to get the connection string

--- Program.cs
+++ Program.cs
@@ -4,12 +4,18 @@ using ranking_app.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = !String.IsNullOrEmpty(builder.Configuration.GetValue<string>("RANKING_APP_CONNECTION_STR"))
+    // If environnement variable is defined for the connection string, use it
+    ? Environment.GetEnvironmentVariable("RANKING_APP_CONNECTION_STR")
+    // Otherwise it means we're in local, so use the connection string defined in the appsettings.json
+    : builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Configure dependency injection for ranking_app_DB postgresql database
 builder.Services.AddDbContext<RankingAppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
