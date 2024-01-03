98b81658c2ecf1a11442febd79cdc1c7b1d8fa51
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2024-01-02 19:28:38 -0500

    TMP: enable ShowPII to facilitate debug

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using ranking_app.Data;
 using System.Net;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -84,6 +85,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+IdentityModelEventSource.ShowPII = true;
 
 // app.UseHttpsRedirection();
 app.UseStaticFiles();
