3f1df6f864cb79bbdadcad3e4037c56b831a373a
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2024-01-04 17:17:21 -0500

    Add secure headers

--- Program.cs
+++ Program.cs
@@ -24,6 +24,13 @@ builder.Services.AddDbContext<RankingAppDbContext>(options =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+// Security : Sets the Strict-Transport-Security header
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+});
+
 // To fix ERR_TOO_MANY_REDIRECTS error
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
@@ -73,6 +80,17 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Middleware to add secure headers
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("X-Frame-Options", "DENY");
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+    context.Response.Headers.Add("Permissions-Policy", "camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), usb=()");
+    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
+    await next();
+});
 
 app.UseStaticFiles();
 app.UseRouting();
